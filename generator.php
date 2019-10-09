640468a57d2cf7917e44970f17b8f721160f64a5
kabir2350 <30516305+kabir2350@users.noreply.github.com>
2019-10-09 03:46:28 -0700

    Create generator.php

---
+++ generator.php
@@ -0,0 +1,63 @@
+
+
+<?php
+
+
+$arr=['id','domain','custom','sortval','dttm','chan','title','link',
+'descr','meta','bool1','bool2','next','prev'];
+
+$arr1= 
+[
+'one', 'two','three','four','five','six','seven','eight','nine', 'ten','eleven','twelve', 'thirteen','forteen'
+
+];
+
+
+for ($i=0; $i <14 ; $i++) { 
+
+
+
+
+	
+
+echo "\$$arr1[$i] , <br />";
+
+
+     
+
+
+
+}/*
+
+
+
+
+echo  "\$$arr1[$i] = filter_input(INPUT_POST, '$arr1[$i]');<br /> ";
+
+echo "\$_POST['$arr[$i]'] ,'_' ,";
+echo "':$arr[$i]' => \$_POST['$arr[$i]'],<br />";
+
+echo "$arr[$i] = :$arr[$i],<br /> ";
+echo "':$arr[$i]' => \$_POST['$arr[$i]'],<br />";
+
+echo "\$_POST['$arr[$i]'] =filter_input(INPUT_POST, '$arr[$i]');<br />";
+echo "\$output['$arr[$i]'] = \$row['$arr[$i]'];<br /> ";
+	echo "var $arr[$i] = $('#$arr[$i]').val();<br />";
+
+echo "$('#$arr[$i]').val(data.$arr[$i]);<br />";
+echo "\$sub_array[] = \$row['$arr[$i]'];<br />  ";
+echo ":$arr[$i], ";
+echo "
+
+\\\\label> $arr[$i]\\\\/label><br />
+     \\\\input type='text' name='$arr[$i]' id='$arr[$i]' class='form-control' /><br />
+     \\\\br />
+
+";
+
+echo " \\\\th width='10%'>  $arr[$i] \\\\/th> <br>  ";
+
+
+*/
+
+
